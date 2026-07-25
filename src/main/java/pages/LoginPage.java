2c348350ebf156609abb47eb0a5490f794410aa6
Ravshana Zakirova <rok.zak@icloud.com>
2026-07-24 17:32:19 -0700

    LoginTest added

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,4 +1,34 @@
 package pages;
 
-public class LoginPage {
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage extends BasePage {
+
+    private final By usernameLocator = By.id("user-name");
+    private final By passwordLocator = By.id("password");
+    private final By loginButtonLocator = By.id("login-button");
+
+    public LoginPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public void enterUsername(String username) {
+        type(usernameLocator, username);
+    }
+
+    public void enterPassword(String password) {
+        type(passwordLocator, password);
+    }
+
+    public void clickLoginButton() {
+        click(loginButtonLocator);
+    }
+
+    public void loginAs(String username, String password) {
+        type(usernameLocator, username);
+        type(passwordLocator, password);
+        click(loginButtonLocator);
+    }
 }
--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -1,4 +1,32 @@
 package base;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+
+    protected WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }

f0768565e267d78834bd3fa582bf477d17066531
semsimecapraz <semsimecapraz@yahoo.com>
2026-07-27 16:56:53 -0400

    Implement shopping cart page and item badge management
    # Conflicts:
    #	src/main/java/pages/CartPage.java

--- src/main/java/pages/CartPage.java
+++ src/main/java/pages/CartPage.java
@@ -5,19 +5,31 @@ import org.openqa.selenium.WebDriver;
 
 public class CartPage extends BasePage {
 
-    //Constructor
     public CartPage(WebDriver driver) {
         super(driver);
     }
 
+    private final By productName = By.className("inventory_item_name");
+    private final By productPrice = By.className("inventory_item_price");
+    private final By productDescription = By.className("inventory_item_desc");
     //Locator
     private final By checkoutBtn = By.id("checkout");
 
+    public String getProductName() {
+        return findElement(productName).getText();
+    }
 
     //Action: Return the next page object for methods that navigate to another page.
     public CheckoutInfoPage goToCheckout() {
         click(checkoutBtn);
         // Navigates to a different page → return the new page object
         return new CheckoutInfoPage(driver);
+    }
+    public String getProductPrice() {
+        return findElement(productPrice).getText();
+    }
+
+    public String getProductDescription() {
+        return findElement(productDescription).getText();
     }
 }

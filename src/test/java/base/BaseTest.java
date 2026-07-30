0dcb19b020a63070708545d660d56f60f0e1f504
Ravshana Zakirova <rok.zak@icloud.com>
2026-07-30 15:59:00 -0700

    Allure reports  set up

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -22,6 +22,7 @@ public class BaseTest {
         driver = new ChromeDriver(BrowserOptions.getOptions());
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
         driver.get(ConfigReader.getProperty("loginUrl"));
 
     }

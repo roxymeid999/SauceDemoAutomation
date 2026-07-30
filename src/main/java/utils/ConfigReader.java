e7969fbc7845bee1e046cebe6a1f8d39c39808d4
Ravshana Zakirova <rok.zak@icloud.com>
2026-07-30 16:05:00 -0700

    Allure reports  set up

--- src/main/java/utils/ConfigReader.java
+++ src/main/java/utils/ConfigReader.java
@@ -1,18 +1,21 @@
 package utils;
 
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigReader {
     private static Properties properties;
 
-
     static {
         try {
+            // Используем ClassLoader для поиска файла в classpath (работает и в IDEA, и в Jenkins)
+            InputStream input = ConfigReader.class.getClassLoader().getResourceAsStream("config.properties");
 
-            String filePath = "src/test/resources/config.properties";
-            FileInputStream input = new FileInputStream(filePath);
+            if (input == null) {
+                throw new RuntimeException("config.properties not found in classpath!");
+            }
+
             properties = new Properties();
             properties.load(input);
             input.close();
@@ -21,7 +24,6 @@ public class ConfigReader {
             throw new RuntimeException("Not able to load the file config.properties!");
         }
     }
-
 
     public static String getProperty(String key) {
         return properties.getProperty(key);

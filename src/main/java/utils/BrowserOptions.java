4a4a0b1c960121c3b0521cc9e939dd44834fe8e9
Ravshana Zakirova <rok.zak@icloud.com>
2026-07-30 16:41:21 -0700

    Allure reports  set up

--- src/main/java/utils/BrowserOptions.java
+++ src/main/java/utils/BrowserOptions.java
@@ -6,25 +6,26 @@ import java.util.Map;
 
 public class BrowserOptions {
 
-    public static ChromeOptions getOptions() {
+        public static ChromeOptions getOptions() {
+                ChromeOptions options = new ChromeOptions();
 
-            ChromeOptions options = new ChromeOptions();
+                Map<String, Object> prefs = new HashMap<>();
+                prefs.put("credentials_enable_service", false);
+                prefs.put("profile.password_manager_enabled", false);
+                options.setExperimentalOption("prefs", prefs);
 
+                options.addArguments("--disable-save-password-bubble");
+                options.addArguments("--disable-notifications");
+                options.addArguments("--disable-features=PasswordManagerOnboarding,AutofillServerCommunication");
+                options.addArguments("--disable-sync");
+                options.addArguments("--incognito");
 
-            Map<String, Object> prefs = new HashMap<>();
-            prefs.put("credentials_enable_service", false);
-            prefs.put("profile.password_manager_enabled", false);
-            options.setExperimentalOption("prefs", prefs);
+                // Обязательные флаги для запуска в Jenkins (без графического интерфейса)
+                options.addArguments("--headless=new");
+                options.addArguments("--no-sandbox");
+                options.addArguments("--disable-dev-shm-usage");
+                options.addArguments("--window-size=1920,1080");
 
-
-            options.addArguments("--disable-save-password-bubble");
-            options.addArguments("--disable-notifications");
-            options.addArguments("--disable-features=PasswordManagerOnboarding,AutofillServerCommunication");
-            options.addArguments("--disable-sync");
-
-
-            options.addArguments("--incognito");
-
-            return options;
+                return options;
         }
 }
